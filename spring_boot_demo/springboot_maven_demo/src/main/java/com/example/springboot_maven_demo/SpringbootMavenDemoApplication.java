75717611a9058dbf7c9abfab0daf16a3c76c6d3d
MiShuaiguang <1045142975@qq.com>
2020-03-18 10:58:49 +0800

    git提交更改测试

--- spring_boot_demo/springboot_maven_demo/src/main/java/com/example/springboot_maven_demo/SpringbootMavenDemoApplication.java
+++ spring_boot_demo/springboot_maven_demo/src/main/java/com/example/springboot_maven_demo/SpringbootMavenDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootMavenDemoApplication {
-
+    int a =10;
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMavenDemoApplication.class, args);
     }
